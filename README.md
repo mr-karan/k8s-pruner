1d97ed2318dfadea68a8e38aa9c9a3025f84e26f
mr-karan <karansharma1295@gmail.com>
2019-11-02 14:47:22 +0530

    feat: cleanup script

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # k8s-pruner
-Cleanup unused Configmaps, Secrets and other resources in a Kubernetes namespace
+Cleanup unused Configmaps in a Kubernetes namespace
+
+## Why
+
+For folks using Kustomize, the configmaps are prefixed with a random hash. Whenever you change the config, the hash is changed which signals the Deployment to rollout new Pods with new configmap and kill the old ones. The problem seems to be that the old ConfigMaps are not garbage collected and seems to be stored in the etcd cluster forever.
+
+## Features
+
+- Runs inside Kubernetes cluster, no need to setup any external systems/bastions.
+- Depends on K8s native RBAC and ServiceAccount for authentication/authorization.
+- Executed as a CronJob
+
+## Deployment
+
+TODO.
+
+## Known Limitations
+
+If you've scaled down a deployment in a namespace, the configmaps will be deleted.
--- prune-cm.sh
+++ prune-cm.sh
@@ -4,6 +4,30 @@ set -o pipefail  # trace ERR through pipes
 set -o errtrace  # trace ERR through 'time command' and other functions
 
 
+dry_run=false
+verbose=false
+programname=$0
+
+# display help text.
+print_usage() {
+    echo "usage: $programname [-dvh]"
+    echo "  -d      enable dry run. Running it with dry run will only list the configmaps to be deleted, it won't actually delete them"
+    echo "  -v      enable verbose logging"
+    echo "  -h      display help (this page)"
+    exit 1
+}
+
+# parse optional args and flags.
+while getopts 'dvh' flag; do
+  case "${flag}" in
+    d) dry_run=true ;;
+    v) verbose=true ;;
+    *) print_usage
+       exit 1 ;;
+  esac
+done
+
+# sends a message to webhook URL with body `{text:...}` in case the script fails at any point.
 function alertOnFailure(){
     if [ ! -z "$ALERT_WEBHOOK_URL" ]; then
         echo "I am set"
@@ -11,6 +35,7 @@ function alertOnFailure(){
     fi
 }
 
+# error() is called everytime any command returns a non zero exit code.
 function error() {
     JOB="$0"              # job name
     LASTLINE="$1"         # line of error occurrence
@@ -21,31 +46,11 @@ function error() {
 }
 trap 'error ${LINENO} ${?}' ERR
 
+# set default variables.
+# KUBE_TOKEN=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
+# KUBE_API_URL="https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/api"
 
-a_flag=''
-b_flag=''
-files=''
-verbose='false'
-
-print_usage() {
-  printf "Usage: ..."
-}
-
-while getopts 'abf:v' flag; do
-  case "${flag}" in
-    a) a_flag='true' ;;
-    b) b_flag='true' ;;
-    f) files="${OPTARG}" ;;
-    v) verbose='true' ;;
-    *) print_usage
-       exit 1 ;;
-  esac
-done
-
-
-KUBE_TOKEN=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
-KUBE_API_URL="https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/api"
-
+# check if all required variables are set.
 function checkRequiredVariables() {
     if [ -z "$KUBE_TOKEN" ]; then
         echo "Missing required value of KUBE_TOKEN"
@@ -53,45 +58,44 @@ function checkRequiredVariables() {
     fi
 }
 
+# fetches all namespaces. Namespaces containing `kube*` are filtered out since they are usually not managed
+# by the end user but system.
 function getAllNamespaces() {
     echo "Fetching all namespaces"
-    # Filter out all kube* namespaces since they are actually not managed by end users.
-    ALL_NAMESPACES=( $(curl -sSk -H "Authorization: Bearer $KUBE_TOKEN" --url "${KUBE_API_URL}/v1/namespaces"  |jq -r '.items[].metadata | select(.name | contains("kube") | not) | .name') )
+    ALL_NAMESPACES=( $(curl -sSk -H "Authorization: Bearer $KUBE_TOKEN" --url "${KUBE_API_URL}/v1/namespaces"  |jq -r '.items[].metadata | select(.name | startswith("kube") | not) | .name') )
 }
 
-# function filterNamespaces() {
-
-# }
-
+# given a namespace, it fetches all configmaps existing.
 function getAllConfigMaps() {
     echo "Fetching all configmaps listed in namespace $1"
     ALL_CONFIGMAPS=( $(curl -sSk -H "Authorization: Bearer $KUBE_TOKEN" --url "${KUBE_API_URL}/v1/namespaces/${1}/configmaps" | jq -r '.items[].metadata.name') )
 }
 
+# given a namespace, it fetches all pods and currently used configmaps.
 function getActiveConfigMaps() {
     echo "Fetching only active configmaps listed in namespace $1"
     USED_CONFIGMAPS=( $(curl -sSk -H "Authorization: Bearer $KUBE_TOKEN" --url "${KUBE_API_URL}/v1/namespaces/${1}/pods" | jq -r '.items[].spec.volumes[]?.configMap.name' | grep -v null || true | sort | uniq) )
 }
 
+# given an array of all configmaps and used configmaps it extracts the unused configmaps.
 function getInactiveConfigMaps() {
     UNUSED_CONFIGMAPS=(`echo ${ALL_CONFIGMAPS[@]} ${USED_CONFIGMAPS[@]} | tr ' ' '\n' | sort | uniq -u`)
 }
 
+# given a configmap name it removes from the namespace
 function pruneInactiveConfigMaps() {
     for cm in "${UNUSED_CONFIGMAPS[@]}"
     do
         echo "Deleting unused configmap $cm present in namespace $1"
-        # kubectl delete configmap $cm -n $1
+        if [ "$dry_run" != true ] ; then
+            kubectl delete configmap $cm -n $1
+        fi
     done
 }
 
+
 checkRequiredVariables
-
-# fetch list of namespaces
 getAllNamespaces
-# filterNamespaces
-
-echo $ALL_NAMESPACES
 
 for ns in "${ALL_NAMESPACES[@]}"
 do
