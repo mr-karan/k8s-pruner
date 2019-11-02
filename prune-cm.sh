0eea72eb10c6e048e130c000400de93461d473c9
Karan Sharma <karansharma1295@gmail.com>
2019-11-02 07:29:24 +0530

    Update prune-cm.sh

--- prune-cm.sh
+++ prune-cm.sh
@@ -3,19 +3,55 @@
 set -o pipefail  # trace ERR through pipes
 set -o errtrace  # trace ERR through 'time command' and other functions
 
+
+function alertOnFailure(){
+    if [ ! -z "$ALERT_WEBHOOK_URL" ]; then
+        echo "I am set"
+        curl --header "Content-Type: application/json" --request POST --data '{"text":"Error while pruning unused configmaps"}' $ALERT_WEBHOOK_URL
+    fi
+}
+
 function error() {
     JOB="$0"              # job name
     LASTLINE="$1"         # line of error occurrence
     LASTERR="$2"          # error code
     echo "ERROR in ${JOB} : line ${LASTLINE} with exit code ${LASTERR}"
+    alertOnFailure
     exit 1
 }
 trap 'error ${LINENO} ${?}' ERR
 
+
+a_flag=''
+b_flag=''
+files=''
+verbose='false'
+
+print_usage() {
+  printf "Usage: ..."
+}
+
+while getopts 'abf:v' flag; do
+  case "${flag}" in
+    a) a_flag='true' ;;
+    b) b_flag='true' ;;
+    f) files="${OPTARG}" ;;
+    v) verbose='true' ;;
+    *) print_usage
+       exit 1 ;;
+  esac
+done
+
+
 function getAllNamespaces() {
     echo "Fetching all namespaces"
-    KUBE_NAMESPACES=( $(kubectl get ns -o json | jq -r '.items[].metadata.name' | grep -v 'kube') )
+    # Filter out all kube* namespaces since they are actually not managed by end users.
+    ALL_NAMESPACES=( $(kubectl get ns -o json |jq -r '.items[].metadata | select(.name | contains("kube") | not) | .name') )
 }
+
+# function filterNamespaces() {
+
+# }
 
 function getAllConfigMaps() {
     echo "Fetching all configmaps listed in namespace $1"
@@ -41,8 +77,9 @@ function pruneInactiveConfigMaps() {
  
 # fetch list of namespaces
 getAllNamespaces
+# filterNamespaces
 
-for ns in "${KUBE_NAMESPACES[@]}"
+for ns in "${ALL_NAMESPACES[@]}"
 do
     getAllConfigMaps $ns
     getActiveConfigMaps $ns
